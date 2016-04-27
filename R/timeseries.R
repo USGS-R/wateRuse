4618b86390c82a5d93be0089f1b49b6e3efe1929
Shourds <jshourds@usgs.gov>
2016-04-27 10:19:44 -0500

    Added a time series stubbing

---
+++ R/timeseries.R
@@ -0,0 +1,20 @@
+#' time.series.data
+#'
+#' time.series.data
+#' 
+#' @param data.element character name of data element within available categories by year for state
+#' @param years vector of integers specifying all years available for state
+#
+#'
+#' 
+#' @export
+#' 
+#' @examples 
+#' 
+#' data.element <- "PS-GWPop"
+#' time_series_data(data.element)
+time_series_data <- function(data.element, years= NA){
+  
+  return(NA)
+}
+  
